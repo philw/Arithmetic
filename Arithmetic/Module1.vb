2a361511c345aebfa030686c01eea2ffe94ad0ab
Phil Wood <pwood@strode-college.ac.uk>
2017-10-03 10:15:42 +0100

    Just diplay menu

--- Arithmetic/Module1.vb
+++ Arithmetic/Module1.vb
@@ -2,6 +2,17 @@
 
     Sub Main()
 
+        DisplayMenu()
+
+
+
+        Console.ReadLine()
+    End Sub
+
+    Sub DisplayMenu()
+        Console.WriteLine("1. Show times table")
+        Console.WriteLine("2. Times table test")
+        Console.WriteLine("3. Exit program")
     End Sub
 
 End Module
